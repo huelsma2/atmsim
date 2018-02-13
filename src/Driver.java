077b2693f241438beea0973d41df14657a88bf28
huelsma2 <huelsma2@ceas-942-mac13.cae.uwm.edu>
2018-02-13 12:15:01 -0600

    Driver

--- src/Driver.java
+++ src/Driver.java
@@ -1,6 +1,12 @@
 import java.util.HashMap;
+import java.util.Scanner;
 
-
+/**
+ * Test driver which creates a bank with a map of accounts, and simulates a Customer putting their card
+ * in a machine.
+ * @author Andrew Huelsman
+ *
+ */
 public class Driver {
 
 	static Bank _bank;
@@ -9,22 +15,16 @@ public class Driver {
 		HashMap<Integer, Account> accountList = new HashMap<Integer, Account>();
 		accountList.put(1234, new Account(1234, 6789, 80));
 		accountList.put(6789, new Account(6789, 4321, 60));
-		_bank = new Bank(accountList);//
-	}
-	
-	
-	private boolean sendForValidation(Card number)
-	{
-		return _bank.validate(number);
-	}
-	
-	private boolean isValidPin(Card in, int input)
-	{
-		return _bank.validate(in, input);
-	}
-	
-	private Account getAccount(Card in)
-	{
-		return _bank.getAccount(in);
+		_bank = new Bank(accountList);
+		Scanner stdin = new Scanner(System.in);
+		while(true)
+		{
+			int accnum;
+			try{accnum= stdin.nextInt();} catch (NumberFormatException e) { System.out.println("Invalid format"); }
+			new ATM(_bank).start(accnum);
+			System.out.println("Transaction finished. Continue? (y for yes, anything else for no");
+			if(!stdin.next().equals("y"))
+				break;
+		}
 	}
 }
