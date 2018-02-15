9bf021df6addfe0f45d8a6c1e13e4b62a9d9975c
Michael Davis <Michael Davis@Michael-Laptop.mshome.net>
2018-02-15 10:41:34 -0600

    Removed unneeded package imports

--- src/BankTest.java
+++ src/BankTest.java
@@ -8,11 +8,6 @@ import static org.junit.Assert.*;
 import java.util.HashMap;
 
 import org.junit.Test;
-import org.junit.internal.runners.JUnit38ClassRunner;
-
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
-
 
 
 public class BankTest{
