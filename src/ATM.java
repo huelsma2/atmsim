9b58d633876b61274fec245ea156f0872d8fb98a
huelsma2 <huelsma2@ceas-942-mac13.cae.uwm.edu>
2018-02-20 12:25:32 -0600

    g

--- src/ATM.java
+++ src/ATM.java
@@ -1,9 +1,12 @@
 /**
  * ATM class simulates the ATM that a customer/actor will interact with.
  * ATM takes the input account number, validates, transacts, and prints receipt.
- * @author Andrew Yehle
+ * @author Andrew Yehle, Andrew Huelsman
  */
 
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.Scanner;
 
 public class ATM {
@@ -12,6 +15,14 @@ public class ATM {
 		private Card testCard;
 		private int PIN;
 		private int amount = 0;
+		private Printer _printer;
+		private CashDispenser _cashDispenser;
+		private CardReader _cardReader;
+		
+		private static final String[] COMMANDS = {"CARDREAD", "NUM", "DIS", "PRINT", "BUTTON"};
+		
+		private static enum STATES {NOCUSTOMER, TAKENUMBER, TAKEBUTTON};
+		private STATES state;
 		
 		/**
 		 * Creates an ATM object that takes an input of a Bank object.
@@ -20,7 +31,7 @@ public class ATM {
 		public ATM(Bank newBank){
 			
 			this.testBank = newBank;
-			
+			state = STATES.NOCUSTOMER;
 		}
 		
 		/**
@@ -94,6 +105,60 @@ public class ATM {
 			execute(testCard, transaction,input);
 		}else{
 			return;
+			}
 		}
-	}
+		
+		public void runCommand(String[] command)
+		{
+			boolean hasTime = command.length==3;
+			switch(hasTime? command[1].toUpperCase() : command[0].toUpperCase())
+			{
+			case "CARDREAD":
+			{
+				if(state != STATES.NOCUSTOMER) 
+				{
+					System.out.println("I'm already reading a card, asshole");
+					break;
+				}
+				if(hasTime) printTime(command[0]); else printTime();
+				try{ 
+					testCard = new Card(_cardReader.acctNumber(Integer.parseInt(hasTime? command[2] : command[1])));
+					System.out.println("Card read. Account number: " + testCard.accountNumber());
+					state = STATES.TAKENUMBER;
+					if(hasTime) printTime(command[0]); else printTime();
+					System.out.println("Enter Pin:");
+				}
+				catch(NumberFormatException e){System.out.println("Invalid Card...");}
+				break;
+			}
+			case "NUM":
+			{
+				if(state != STATES.TAKENUMBER) 
+				{
+					System.out.println("Don't you put numbers in me when I'm not ready, asshole");
+					break;
+				}
+				if(hasTime) printTime(command[0]); else printTime();
+				try{ 
+					testCard = new Card(_cardReader.acctNumber(Integer.parseInt(hasTime? command[2] : command[1])));
+					System.out.println("Card read. Account number: " + testCard.accountNumber());
+					state = STATES.TAKENUMBER;
+				}
+				catch(NumberFormatException e){System.out.println("Invalid Card...");}
+				break;
+			}
+				
+			}
+		}
+
+		private void printTime()
+		{
+			Timestamp ts = new Timestamp(new Date().getTime());
+			System.out.print(ts.toString().split(" ")[1].split(".")[0]);
+		}
+		
+		private void printTime(String t)
+		{
+			System.out.print(t);
+		}
 }
