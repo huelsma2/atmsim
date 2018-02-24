5e63ad4f03a9dbbe6abf596615a299b464b5807c
Michael Davis <Michael Davis@Michael-Laptop.mshome.net>
2018-02-24 17:39:17 -0600

    ;-)

--- src/ATMTest.java
+++ src/ATMTest.java
@@ -16,7 +16,16 @@ public class ATMTest{
 	static Bank _bank;
 	static Account _account;
 	static ATM _atm;
+	static CardReader _cardreader;
+	static CashDispenser _cashdispenser;
+	static Printer _printer;
 	
+	
+	
+	Card card1 = new Card(1234);
+	Card card2 = new Card(6789);
+	Card card3 = new Card(1111); //Bad account Value 
+	Card card4 = new Card(12345); //Bad account Value
 	
 	/**
 	 * Adds accounts in the following HashMap
@@ -46,16 +55,9 @@ public class ATMTest{
 	accountList.put(6789, new Account(6789, 4321, 60.00));
 	_bank = new Bank(accountList);
 	_atm = new ATM(_bank);
-	//card read commands
-	String NUM;
- 	String DIS;
- 	String PRINT;
- 	
- 	//Button Commands
- 	String W;
- 	String CB;
- 	String CANCEL;
- 	String D;
+	_printer = new Printer();
+	_cashdispenser = new CashDispenser();
+	_cardreader = new CardReader();
  }
 
 	
@@ -66,18 +68,26 @@ public class ATMTest{
 	@Test
 	public void TestValidAccountNumber() {
 		setup();
-		 _atm.runCommand("CARDREAD");
+		//_cardreader.acctNumber(card1);
+		_atm.runCommand("CARDREAD " +  Integer.toString(_cardreader.acctNumber(card1)));
 	
 	}
 	
 	/** 
-	 * Tests if card3's account # is invalid to an account
-	 * Tests if card4's account # is invalid to an account
+	 * Tests to see if current time is printed to console.
 	 */
+	@Test
+	public void TestPrintTime() {
+		setup();
+		
+		_atm.printTime();
+	}
+	
 	@Test
 	public void TestInvalidAccountNumber() {
 		setup();
-		
+		assertFalse(_bank.validate(card3));
+		assertFalse(_bank.validate(card4));
 	
 	}
 	
@@ -88,7 +98,8 @@ public class ATMTest{
 	@Test
 	public void CheckValidAccountIncorrectPin() {
 		setup();
- 
+		assertFalse(_bank.validate(card1, 1235));
+		assertFalse(_bank.validate(card2, 6781)); 
 	
 	}
 	
@@ -100,7 +111,9 @@ public class ATMTest{
 	@Test
 	public void TestBalanceOfEachAccount() {
 		setup();
-
+		assertEquals(80.00, _bank.getBalance(card1), 0); 
+		assertEquals(60.00, _bank.getBalance(card2), 0); 	
+	
 	}
 	
 	/** 
@@ -110,6 +123,11 @@ public class ATMTest{
 	@Test
 	public void TestDeposit() {
 		setup();
+		assertTrue(_bank.deposit(card1, 10.00));
+		assertEquals(90.00, _bank.getBalance(card1), 0); 
+	
+		assertTrue(_bank.deposit(card2, 10.00)); 
+		assertEquals(70.00, _bank.getBalance(card2), 0); 
 	
 	}
 	
@@ -121,9 +139,14 @@ public class ATMTest{
 	@Test
 	public void TestWithdraw() {
 		setup();
-
+		assertTrue(_bank.withdraw(card1, 30.00)); 
+		assertEquals(50.00, _bank.getBalance(card1), 0); 
+	
+		assertTrue(_bank.withdraw(card2, 50.00)); 
+		assertEquals(10.00, _bank.getBalance(card2), 0); 
 	
 	}
+	
 	/** 
 	 * Tests Over withdrawing from card1 of 120.00 when balance is 60.00
 	 * Tests Over withdrawing from card2 of 60.00 when balance is 20.00
@@ -131,7 +154,12 @@ public class ATMTest{
 	@Test
 	public void OverWithDraw() {
 		setup();
-
+		assertFalse(_bank.withdraw(card1, 120.00)); 
+		assertEquals(80.00, _bank.getBalance(card1), 0); 
+		
+		assertFalse(_bank.withdraw(card2, 70.00)); 
+		assertEquals(60.00, _bank.getBalance(card2), 0); 
+	
 	}
 	
 	/** 
@@ -143,10 +171,12 @@ public class ATMTest{
 	@Test
 	public void WithDrawNegative() {
 		setup();
-
+		assertFalse(_bank.withdraw(card1, -100.00)); 
+		assertEquals(80.00, _bank.getBalance(card1), 0); 
+		
+		assertFalse(_bank.withdraw(card2, -200.00)); 
+		assertEquals(60.00, _bank.getBalance(card2), 0); 
+	
 	}
-	
-
-	
 	
 }
